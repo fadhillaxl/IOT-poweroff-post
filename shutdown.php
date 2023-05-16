09153e6711cb4f60ad4476fe50cda1a8c8f557b0
fadhillaxl <99535995+fadhillaxl@users.noreply.github.com>
2023-05-16 08:27:03 +0000

    shutdown php add

--- shutdown.php
+++ shutdown.php
@@ -5,8 +5,15 @@ $output=null;
 $retval=null;
 exec('shutdown /s', $output, $retval);
 // echo "Returned with status $retval and output: $output";
-$res = "Returned with status $retval and output: $output";
+// $res = "Returned with status $retval and output: ";
 // print_r($output);
+
+
+if($retval == 0){
+    $res = "Command Berhasil";
+}else{
+    $res = "Command Gagal";
+}
 
 $json = json_encode($res);
 echo $json;
